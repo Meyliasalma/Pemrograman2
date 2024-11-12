d3755303696459ac6b641e6e56d03e0957a62fc5
Meyliasalma <162813299+Meyliasalma@users.noreply.github.com>
2024-11-12 21:26:21 +0700

    Add files via upload

---
+++ Tugass_1/MasterDosen.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugass_1;
+
+/**
+ *
+ * @author USER
+ */
+public class MasterDosen {
+    String DosenPengampu = " Tito Prabowo,M.Kom ";
+    String DosenWali = " Vion Age Tricahyo,M.T.I ";
+}
