37ad817f6c3f6d5e50c896800aa6db93162c2646
Meyliasalma <162813299+Meyliasalma@users.noreply.github.com>
2024-12-18 10:09:10 +0700

    Add files via upload

---
+++ UTS_Pemrograman2/pelanggan.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UTS_Pemrograman2;
+
+/**
+ *
+ * @author USER
+ */
+public class pelanggan {
+     private String nama;
+        private String alamat;
+}
